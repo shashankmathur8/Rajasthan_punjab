d641fbbc9f26dd02ab1a894eb98833da3c55a9ee
shubham11408139 <shubhamgupta.608@rediffmail.com>
2018-03-26 00:55:50 +0530

    new-feature completed!

--- Raj4.0/new.js
+++ Raj4.0/new.js
@@ -1,8 +1,11 @@
-function something(){
+function aBigNewFeature(){
 console.log("heeloo bro")
+console.log("Writing a lot of code");
+console.log("finally completed done with this new feature now.");
+};
+function something(){
+	console.log("My name is shubham!");
 }
+
 something();
-hjkknhksjk
-sdjaskj
-jhsdjhjsdh
-checkout delete
+aBigNewFeature();
